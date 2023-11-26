60b1aa76492fee0c2ebeaf3e91d8e64102919ff6
laravelBasic <pipicanodayana2005@gmail.com>
2023-11-25 23:39:46 -0500

    Filtro temporada

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,24 +12,24 @@ use Illuminate\Http\Response;
 
 class ProductController extends Controller
 {
-   //----CRUD PRODUCTO
+    //----CRUD PRODUCTO
 
-   public function index()
-   {
-       // Obtener la lista de productos con información relacionada
-       $products = Product::with('temporada')->get();
-   
-       // Modificar la URL de la imagen para incluir el dominio
-       foreach ($products as $product) {
-           if ($product->image) {
-               $product->image = asset('storage/product/' . $product->image);
-           }
-       }
-   
-       // Devolver la respuesta con todos los datos, incluida la URL completa de la imagen
-       return response()->json($products, Response::HTTP_OK);
-   }
-   
+    public function index()
+    {
+        // Obtener la lista de productos con información relacionada
+        $products = Product::with('temporada')->get();
+
+        // Modificar la URL de la imagen para incluir el dominio
+        foreach ($products as $product) {
+            if ($product->image) {
+                $product->image = asset('storage/product/' . $product->image);
+            }
+        }
+
+        // Devolver la respuesta con todos los datos, incluida la URL completa de la imagen
+        return response()->json($products, Response::HTTP_OK);
+    }
+
     public function store(Request $request)
     {
         $request->validate([
@@ -38,35 +38,35 @@ class ProductController extends Controller
             'image' => 'required|image|mimes:jpeg,png,jpg,gif',
             'price' => 'required|integer',
             'concentration' => 'required|integer',
-            'idSeason' => 'required|integer' 
+            'idSeason' => 'required|integer'
         ]);
-    
+
         try {
-     
+
             if ($request->hasFile('image')) {
                 $imageFile = $request->file('image');
-                
+
                 // Generar un nombre de archivo único basado en la marca de tiempo y la extensión original
                 $imageName = time() . '.' . $imageFile->getClientOriginalExtension();
-            
-               
+
+
                 $imageFile->storeAs('public/product', $imageName);
             }
-    
+
             // Crear el producto en la base de datos
             $product = Product::create($request->except('image') + ['image' => $imageName]);
-    
+
             // Asignar el nombre del archivo al atributo 'image' del modelo de producto (para la respuesta JSON)
             $product->image = $imageName;
-    
+
             // Devolver la respuesta con el nombre de la imagen asignado
             return response()->json($product, Response::HTTP_CREATED);
         } catch (\Exception $e) {
-           
+
             return response()->json(['error' => 'Error al guardar el producto.'], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
-    
+
 
 
 
@@ -82,9 +82,9 @@ class ProductController extends Controller
         // Responder con el producto modificado
         return response()->json($product, Response::HTTP_OK);
     }
- 
-   
- 
+
+
+
 
     public function update(Request $request, Product $product)
     {
@@ -96,36 +96,36 @@ class ProductController extends Controller
             'idSeason' => 'required|integer',
             'image' => 'nullable|image|mimes:jpeg,png,jpg,gif',
         ]);
-    
+
         try {
             $response = null; // Definir la variable $response antes del bloque try
-    
+
             if ($request->hasFile('image')) {
                 $imageFile = $request->file('image');
-    
+
                 // Generar un nombre de archivo único basado en la marca de tiempo y la extensión original
                 $imageName = time() . '.' . $imageFile->getClientOriginalExtension();
-    
+
                 // Almacenar la nueva imagen en la carpeta 'public/product'
                 $imageFile->storeAs('public/product', $imageName);
-    
+
                 // Eliminar la imagen anterior si existe
                 if ($product->image) {
                     Storage::delete('public/product/' . $product->image);
                 }
-    
+
                 // Actualizar el modelo con la nueva información, incluido el nombre de la nueva imagen
                 $product->update($request->except('image') + ['image' => $imageName]);
-    
+
                 // Asignar el nombre del archivo al atributo 'image' del modelo de producto (para la respuesta JSON)
                 $product->image = $imageName;
-    
+
                 // Devolver la respuesta con la información actualizada
                 $response = response()->json($product, Response::HTTP_OK);
             } else {
                 // Si no se proporciona una nueva imagen, simplemente actualizar el modelo con la información existente
                 $product->update($request->except('image'));
-    
+
                 // Devolver la respuesta con la información actualizada
                 $response = response()->json($product, Response::HTTP_OK);
             }
@@ -133,101 +133,77 @@ class ProductController extends Controller
             // Capturar cualquier excepción y devolver un mensaje de error
             $response = response()->json(['error' => 'Error al actualizar el producto.'], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
-    
+
         return $response; // Devolver la respuesta después del bloque try
     }
-    
 
-    public function edit($id){
+
+    public function edit($id)
+    {
         $product = Product::with('temporada')->find($id);
-    
+
         if (!$product) {
             return response()->json(['error' => 'Usuario no encontrado'], Response::HTTP_NOT_FOUND);
         }
-    
+
         // Obtener la URL base del almacenamiento público
         $baseUrl = asset('storage/product/');
-    
+
         // Si la imagen existe, construir la URL completa
         if ($product->image) {
             $product->image = $baseUrl . '/' . $product->image;
         }
-    
+
         return response()->json($product, Response::HTTP_OK);
     }
-    
-
-    
-     public function destroy(Product $product)
-     {
-         try {
-          
-             if ($product->image) {
-                 Storage::delete('public/product/' . $product->image);
-             }
-     
-            
-             $product->delete();
-     
-             
-             return response()->json(null, Response::HTTP_NO_CONTENT);
-         } catch (\Exception $e) {
-          
-             return response()->json(['error' => 'Error al eliminar el producto.'], Response::HTTP_INTERNAL_SERVER_ERROR);
-         }
-     }
-     
 
 
 
-
-
-
-
-
-
-
-
-    public function primavera()
+    public function destroy(Product $product)
     {
-        $productosPrimavera = Product::where('idSeason', 1)->get();
+        try {
+
+            if ($product->image) {
+                Storage::delete('public/product/' . $product->image);
+            }
+
+
+            $product->delete();
+
+
+            return response()->json(null, Response::HTTP_NO_CONTENT);
+        } catch (\Exception $e) {
+
+            return response()->json(['error' => 'Error al eliminar el producto.'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+
+
+//FILTRO TEMPORADA
+
+    public function productTemporada($seasonId)
+    {
+        // Verifica si se proporcionó un ID de temporada válido
+        if (!$seasonId || !Season::find($seasonId)) {
+            return response()->json(['error' => 'Season not found'], 404);
+        }
     
-      
+        // Filtra los productos según la temporada seleccionada y carga la relación 'temporada'
+        $products = Product::with('temporada')->where('idSeason', $seasonId)->get();
     
-        return view('product.primavera', ['productosPrimavera' => $productosPrimavera]);
+
+        foreach ($products as $product) {
+            if ($product->image) {
+              
+                $product->image = asset('storage/product/' . $product->image);
+            }
+        }
+    
+        return response()->json($products);
     }
     
-    public function verano()
-    {
-        $productosVerano = Product::where('idSeason', 2)->get(); 
-    
-      
-    
-        return view('product.verano', ['productosVerano' => $productosVerano]);
-    }
-    public function otoño()
-    {
-        $productosOtoño = Product::where('idSeason', 3)->get(); 
-    
-      
-    
-        return view('product.otoño', ['productosOtoño' => $productosOtoño]);
-    }
-    public function invierno()
-    {
-        $productosInvierno = Product::where('idSeason', 4)->get(); 
-    
-      
-    
-        return view('product.invierno', ['productosInvierno' => $productosInvierno]);
-    }
-    
-    
-
-
-  
-
-
 }
 
 
--- routes/api.php
+++ routes/api.php
@@ -28,7 +28,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 
-//RUTAS TEMPORADA
+//RUTAS TEMPORADA CRUD
 
 Route::post('/temporada/store', [SeasonController::class,'store'])->name('season.store');
 Route::get('temporada/create',[SeasonController::class,'create'])->name('season.create');
@@ -39,7 +39,7 @@ Route::put('/temporada/update/{season}',[SeasonController::class, 'update'])->na
 Route::get('/temporada/show/{season}',[SeasonController::class, 'show'])->name('season.show');
 
 
-//RUTAS ROL
+//RUTAS ROL 
 /* 
 Route::post('/rol/store', [RolController::class,'store'])->name('rol.store');
 Route::get('/rol/create',[RolController::class,'create'])->name('rol.create'); */
@@ -50,7 +50,7 @@ Route::put('/rol/update/{rol}',[RolController::class, 'update'])->name('rol.upda
 Route::get('/rol/show',[RolController::class, 'show'])->name('rol.show');
  */
 
-//RUTAS USUARIO
+//RUTAS USUARIO CRUD
 
 
 Route::post('/usuario/store',[UserController::class,'store'])->name('user.store');
@@ -61,14 +61,13 @@ Route::get('/usuario/edit/{id}',[UserController::class, 'edit'])->name('user.edi
 /* Route::put('/usuario/update/{user}',[UserController::class, 'update'])->name('update.crud'); */
 Route::get('/usuario/show',[UserController::class, 'show'])->name('user.show');
 
-Route::post('register', [AuthController::class,'register'])->name('register');
-Route::post('logins', [AuthController::class,'logins'])->name('logins');
 
 
 
 
 
-//RUTAS DE PRODUCTOS
+//RUTAS DE PRODUCTOS CRUD
+
 Route::post('/producto/store', [ProductController::class,'store'])->name('product.store');
 Route::get('/producto/create',[ProductController::class,'create'])->name('product.create');
 Route::get('productos',[ProductController::class,'index'])->name('product.index'); 
@@ -78,14 +77,17 @@ Route::put('/producto/update/{product}',[ProductController::class, 'update'])->n
 Route::get('/producto/show/{product}',[ProductController::class, 'show'])->name('product.show');
 
 
+//TEMPORADAS FILTRO
 
 
+Route::get('/producto/temporada/{seasonId}', [ProductController::class, 'productTemporada'])->name('temporadas');
 
 
+//AUTENTICACIONES
 
+Route::post('register', [AuthController::class,'register'])->name('register');
+Route::post('logins', [AuthController::class,'logins'])->name('logins');
 
-
-//AUTHENTICACIONES
 
  Route::middleware(['auth:sanctum'])->group(function(){
     Route::put('/usuario/update',[AuthController::class, 'updateProfile'])->name('user.update');
@@ -103,10 +105,5 @@ Route::get('/producto/show/{product}',[ProductController::class, 'show'])->name(
 
 
 
-//TEMPORADAS
 
 
-Route::get('/producto/primavera', [ProductController::class,'primavera'])->name('primavera');
-Route::get('/producto/verano', [ProductController::class,'verano'])->name('verano');
-Route::get('/producto/otoño', [ProductController::class,'otoño'])->name('otoño');
-Route::get('/producto/invierno', [ProductController::class,'invierno'])->name('invierno');
