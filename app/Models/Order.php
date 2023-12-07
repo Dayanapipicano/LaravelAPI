5394d842718a1c7655ddf96583bf5215bd6701f7
laravelBasic <pipicanodayana2005@gmail.com>
2023-12-06 22:48:25 -0500

    Pedido

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -11,11 +11,14 @@ class Order extends Model
     protected $fillable = ['idShoppingCart', 'idTypePay'];
 
     use HasFactory;
-    public function shoppingCart(){
-        return $this->belongsTo('App\Models\ShoppingCart');
+    public function shoppingCart()
+    {
+        return $this->belongsTo('App\Models\ShoppingCart', 'idShoppingCart');
     }
-    public function typePay(){
-        return $this->belongsTo('App\Models\Pay');
+
+    public function typePay()
+    {
+        return $this->belongsTo('App\Models\TypePay', 'idTypePay');
     }
     public function devolucion(){
         return $this->belongsTo('App\Models\Returns');
